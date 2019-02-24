d63520ceb7353d38bd94e27a57ac37e995e7c3e1
Skylar Wurster <swwurster@gmail.com>
2019-02-24 16:44:14 -0500

    Spacialized audio for player frostbolt

--- Assets/Scripts/Entity.Projectiles/EnemyProjectile.cs
+++ Assets/Scripts/Entity.Projectiles/EnemyProjectile.cs
@@ -19,7 +19,7 @@ public class EnemyProjectile : MonoBehaviour {
     protected Rigidbody2D rb;
     protected Animator anim;
     	
-	protected virtual void Update () {
+	public virtual void Update () {
         if (!facingCorrectSide)
         {
             rb = gameObject.GetComponent<Rigidbody2D>();
--- Assets/Scripts/Entity.Projectiles/FrostBlastPlayerScript.cs
+++ Assets/Scripts/Entity.Projectiles/FrostBlastPlayerScript.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FrostBlastPlayerScript : Projectile {    
+public class FrostBlastPlayerScript : Projectile {
+
+    public AudioClip persistantClip, explosionClip;
+    AudioSource audioSource;
+
     protected void Awake()
     {
         damage = 100;
@@ -9,5 +13,28 @@ public class FrostBlastPlayerScript : Projectile {
         dissipateSpeed = 0.15f;
         description = "Ball of ice";
         type = "ice";
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.rolloffMode = AudioRolloffMode.Linear;
+        audioSource.maxDistance = 10;
+        audioSource.spatialize = true;
+        audioSource.SetCustomCurve(AudioSourceCurveType.SpatialBlend,
+            AnimationCurve.Linear(0, 0, audioSource.maxDistance, 1));
+        audioSource.clip = persistantClip;
+        audioSource.loop = true;
+        audioSource.playOnAwake = true;
+        audioSource.Play();
+    }
+    
+    protected override void OnTriggerEnter2D(Collider2D c)
+    {
+        base.OnTriggerEnter2D(c);
+        if (hit)
+        {
+            audioSource.Stop();
+            audioSource.clip = explosionClip;
+            audioSource.loop = false;
+            audioSource.time = 0;
+            audioSource.Play();
+        }
     }
 }
