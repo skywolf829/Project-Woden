f1a90cafd91fd8e98a24fc19b351e6c51cab1546
Skylar Wurster <swwurster@gmail.com>
2019-03-13 22:58:53 -0400

    Updates to items

---
+++ Assets/Scripts/Items/IItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IItem
+{
+
+    void PickUp();
+    void Drop();
+    void Purchase();
+    void Sell();
+    void Use();
+    GameObject Create();
+}
